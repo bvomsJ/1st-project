cd0115a2742f046017f3f2006887e0ece4816bce
Alex <example@gmail.com>
2022-05-19 06:56:43 +0300

    Новый урок

--- lesson 2.8/js.js
+++ lesson 2.8/js.js
@@ -1,51 +1,5 @@
-
-if(num < 49) {
-    console.log("Неверно!")
-} else if (num > 100) {
-    console.log("Много!")
-} else {
-    console.log("Верно!")
+function showFirstMessage(text) {
+    alert(text);
 }
 
-(num == 50) ? console.log("Верно!") : console.log("Неверно!");
-
-
-
-switch (num) {
-    case num < 49:
-        console.log("Неверно!");
-        break
-    case num > 100:
-        console.log("Много!");                         //Если какое-то условиеп выполняется, то в консоль пишет соответсвующий текст которое проверило условие и совпало с данными
-        break;                                         //break нужен чтобы отгородить одно условие от другого
-    case num > 80:
-        console.log("Всё ещё много!");
-        break
-    case 50:
-        console.log("Верно!");
-        break;
-    default:
-        console.log("Что-то пошло не так!")
-        break
-}
-
-
-
-let num = 50;
-while (num < 55) {                                                            //Пока наша переменная num меньше 55, мы будем выполнять определенное действие в {}
-    console.log(num);
-    num++;
-}                                    
-
-do {                                                                        //Заставить делать определенное действие - do
-    console.log(num);
-    num++;
-}
-while (num < 55)
-
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        continue
-    }
-    console.log(i)
-}
+showFirstMessage("Hello World!");
